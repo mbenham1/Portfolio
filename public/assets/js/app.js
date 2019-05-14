78586bff59aa2e4440b1b900603ea17105f9ec4e
Benham <Benham@Brets-MacBook-Air.local>
2019-05-14 10:43:34 -0400

    updates

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,26 +5,26 @@ $(document).ready(function () {
     var message = $("#message")
     getPalindromes();
 
-    $("#submit-form").on("click", function(event) {
+    $("#submit-form").on("click", function (event) {
 
         event.preventDefault();
 
         if (!name.val().trim() || !email.val().trim() || !message.val()) {
-            $("#response").text("Please fill out all fields").css({color: "red"});
+            $("#response").text("Please fill out all fields").css({ color: "red" });
             return;
         }
 
         var newFriend = {
 
-            name: name,
-            email: email,
-            message: message
-            // name: $("#name").val(),
-            // email: $("#email").val(),
-            // message: $("#message").val()
+            // name: name,
+            // email: email,
+            // message: message
+            name: $("#name").val(),
+            email: $("#email").val(),
+            message: $("#message").val()
 
         };
-        
+
         submitFriend(newFriend);
 
     })
@@ -33,21 +33,20 @@ $(document).ready(function () {
 
         $.post("/api/friends", friend, function () {
 
-            $("#response").text("Thanks, " + friend.name).css({color: "green"});
+            $("#response").text("Thanks, " + friend.name).css({ color: "green" });
             $("#name").val("");
             $("#email").val("");
             $("#message").val("");
 
         });
-      }
+    }
 
     $("#check").on("click", function (event) {
         event.preventDefault();
         var input = $("#original").val();
         var regEx = /[^A-Za-z]/g;
-        input = input.replace(regEx, '');
-        if (!input || regEx === false) {
-            $("#confirm").text("Invalid entry").css({ color: "red"});
+        if (!input || regEx.test(input)) {
+            $("#confirm").text("Invalid entry").css({ color: "red" });
         } else {
             palindrome(input);
         }
@@ -55,17 +54,20 @@ $(document).ready(function () {
 
     function palindrome(string) {
 
+        var date = new Date();
+        date = moment(date).format("MMMM Do YYYY, h:mm:ss a");
         var original = string.toLowerCase();
         var reverse = original.split('').reverse().join('');
         if (reverse === original) {
             $("#confirm").text("YES, " + original + " is a palindrome!").css({ color: "green" });
 
             var newPalindrome = {
-                palindrome: original
+                palindrome: original,
+                date: date
             };
 
             $.post("/api/palindromes", newPalindrome, function (data) {
-                // console.log(data);
+
             })
 
             // newestPalindrome();
@@ -83,13 +85,18 @@ $(document).ready(function () {
     // }
 
     function getPalindromes() {
-    
-        $.get("/api/palindromes", function (data) {
-                $("#most-recent").empty();
 
-                for (var i=(data.length-5); i < data.length ; i++){
-                $("#most-recent").prepend("'" + data[i].palindrome + "' ");
-        }
+        $.get("/api/palindromes", function (data) {
+            $("#most-recent").empty();
+
+            if (data.length < 5) {
+                $("#most-recent").text("");
+            } else {
+                for (var i = (data.length - 5); i < data.length; i++) {
+                    $("#most-recent").prepend("'" + data[i].palindrome + "' ");
+                }
+            }
+
         })
     }
 
